28bf5ebbe9a5f2b3e48d2eb51a670e87cc3ae327
devppx@gmail.com <devppx@gmail.com>
2023-04-10 07:36:37 +0200

    update readme, add build script for zmk freamework

--- Readme.md
+++ Readme.md
@@ -1,12 +1,40 @@
+
+# Reference
 https://github.com/iasoren/Lily58-Glow-Enc-Wireless-ZMK/blob/master/config/boards/shields/lily58wireless/lily58wireless.dtsi
 
 https://github.com/orvisevans/Lily58-Glow-Enc
 
+https://www.zfrontier.com/app/flow/eNBQAGxWwp0O
 
+https://www.zfrontier.com/app/flow/2ORypKAdzjGY
+
+# Tools
 https://github.com/c0ngnh/ZMK-keymap-editor 
 
-https://nickcoutsos.github.io/keymap-editor/  -- online editor
+## Online editor
+https://nickcoutsos.github.io/keymap-editor/  
 
-https://www.zfrontier.com/app/flow/eNBQAGxWwp0O -- 参考
 
-https://www.zfrontier.com/app/flow/2ORypKAdzjGY -- 参考
+
+https://zmk.dev/docs/troubleshooting
+
+# BLE DEBUG in USB mode
+https://zmk.dev/docs/behaviors/outputs
+
+
+# Encoder
+//  P0.02 - port 0, pin 2 is connected to encoder pin 1 (a)
+//  P0.20 - port 0, pin 20 is connected to encoder 2 (b)
+// EBYTE nRF52840 BLE 5.0 Transceptor inalámbrico 8dbm 120m 2.4GHz E73-2G4M08S1C
+
+
+# Kicad
+
+https://github.com/daprice/keyswitches.pretty
+https://github.com/ruiqimao/keyboard-pcb-guide
+https://github.com/tmk/keyboard_parts.pretty
+https://github.com/egladman/keebs.pretty
+https://github.com/perigoso/keyswitch-kicad-library
+
+
+
---
+++ build.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+start=$(date +%s.%N)
+
+declare -a pids=()
+
+# Set defaults variables
+verbose=false
+quiet=false
+async=true
+shields=("lily58_enc_left" "lily58_enc_right")
+board="nrfmicro_13"
+config_path='/workspaces/zmk/lily58/config'
+zmk_root_path='/workspaces/zmk'
+zmk_app_path='/workspaces/zmk/app'
+pristine_build_flag=''  #clean build flag
+
+# Define usage message function
+usage() {
+    # Loop through the array and concatenate each element with |
+    for item in "${shields[@]}"
+    do
+        shields_text+="$item|"
+    done
+    # Remove the last |
+    shields_text=${shields_text%|}
+
+    echo "Usage: $0 [-h] [-s SHIELD_NAME] [-s BOARD_NAME] [-v]"
+    echo "  -h                      Display this help message"
+    echo "  -s SHIELD_NAME          Pick a specific shields[]. Avaliable shields: $shields_text"
+    echo "  -b BOARD_NAME           Board name, default value [$board]"
+    echo "  -c USER_CONFIG_PATH     ZMK user config path"
+    echo "  -v                      Verbose output (async build will be disabled)"
+    echo "  -p                      Enable pristine build - clean build / do not use previous build caches"
+    echo "  -r                      Build setting reset firmware for split keyboard halves unable to pair issue. Not compatible with -s option"
+    echo "  -i                      Initialize build environment. Run it inside of ZMK docker container."
+    echo "Place this script in zmk root folder."
+    echo "By default, it will run async build for shield  board $board and $shields_text."
+}
+
+init_build_env() {
+    cd $zmk_root_path
+    west init -l app
+    west update
+    # python3 -m pip install remarshal
+    west zephyr-export
+    # git clone git@github.com:devppx/lily58.git 
+    # npm install @actions/artifact     # not sure if it is necessary, delte?
+}
+
+# Parse command line options
+while getopts "hs:c:vpi" opt; do
+    case ${opt} in
+        s )
+            shields=($OPTARG)
+            async=false
+            ;;
+        c )
+            config_path="$OPTARG/config"
+            ;;
+        h )
+            usage
+            exit 0
+            ;;
+        v )
+            verbose=true
+            ;;
+        \? )
+            echo "Invalid option: -$OPTARG" 1>&2
+            usage
+            exit 1
+            ;;
+        p )
+            pristine_build_flag='-p'
+            ;;
+        r )
+            shields=("settings_reset")
+            ;;
+        i )
+            init_build_env
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+timestamp=$(date +%Y-%m-%d_%H-%M-%S)
+for shiled in ${shields[@]}
+do
+    if [[ "$async" = true ]] && [[ "$verbose" = false ]]; then
+          nohup sh -c "cd $zmk_app_path && west build $pristine_build_flag -d build/$shiled -b $board -- -DSHIELD=$shiled -DZMK_CONFIG=$config_path && cp /workspaces/zmk/app/build/$shiled/zephyr/zmk.uf2 /workspaces/zmk/build/$shiled-$timestamp.u2" > $shiled\\build.log 2>&1 &
+        pids+=($!)  # Add the process ID of the first command to the array
+    else 
+        sh -c "cd $zmk_app_path && west build $pristine_build_flag -d build/$shiled -b $board -- -DSHIELD=$shiled -DZMK_CONFIG=$config_path && cp /workspaces/zmk/app/build/$shiled/zephyr/zmk.uf2 /workspaces/zmk/build/$shiled-$timestamp.u2"
+    fi
+done
+
+if [[ $async ]]; then
+    for pid in "${pids[@]}"; do
+        wait $pid
+    done
+fi
+
+end=$(date +%s.%N)
+echo "Timestamp: $timestamp | Time elapsed: $(echo "$end - $start" | bc) seconds "
